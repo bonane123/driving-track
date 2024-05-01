53cb669dc1771344dcb1ebcff80896c0541f3fb9
bonane <ndayishimyebonane@gmail.com>
2024-05-01 23:34:26 +0300

    creating sections

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h2>Driving Track</h2>
-      </header>
+      <div className="App">
+        <div className="section first-section">First Section (10%)</div>
+        <div className="section second-section">Second Section (Rest)</div>
+        <div className="section third-section">Third Section (20%)</div>
+      </div>
     </div>
   );
 }
